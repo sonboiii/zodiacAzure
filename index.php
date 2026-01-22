2fcc9db4ece663c307e1d1e0027b16c79723cbd0
sonboiii <hoangson27203@gmail.com>
2026-01-22 11:39:36 -0800

    added name to index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <div class="container">
     <div class="mt-4 p-2 bg-secondary text-white rounded" style="width: 60%;">
+        <h1>Son Bui</h1>
         <h3>Check your zodiac.</h3>
         <p>
             Enter your birth year to know your Zodiac sign!
